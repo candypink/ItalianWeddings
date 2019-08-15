30d1134eb50f446d892f12a3263316f2e3b4e5df
Candy Pink <candypink@outlook.it>
2019-08-15 12:26:29 +0200

    one entry for each comment

--- matrimonio/spiders/forum.py
+++ matrimonio/spiders/forum.py
@@ -10,23 +10,41 @@ class ForumSpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+            
+    def parse_comment(self,response):
+        print('   chiara: in test_fun!!')
+        i=0
+        for comment in response.xpath('//li[@class="pure-g discuss-post-comment"]'):
+            # take all the text, turn it to lower case and remove extra spaces
+            # ignore 'Visualizza messaggio citato'
+            text = [x.strip().lower() for x in comment.xpath('div//div[contains(@class,"discuss-post-comment-content") and contains(@class,"com-discuss")]/*//text()').getall() 
+                    if 'Visualizza messaggio citato' not in x]
+            # remove empty strings
+            text = list(filter(None, text))
+            # join different paragraphs of same comment
+            text = ' '.join(text)
+            title = 'comment'
+            print(text)
+            yield{
+                'title':title,
+                'text':text
+                }
 
     def parse_topic(self, response):
-        title = response.xpath('//meta[@property="og:title"]/@content').get().strip()
-        text = response.xpath('//div[@class="com-post-content left-center-enabled"]/text()').get().strip()
-        list_text_comments = [x.strip() for x in response.xpath('//div[@class="com-discuss discuss-post-comment-content comment-item-comment left-center-enabled"]/div/text()').getall()]
-        list_date_comments = response.xpath('//div[@class="discuss-post-comment-header"]//time/text()').getall()
+        print("chiara: parsing topic")
+        title = response.xpath('//meta[@property="og:title"]/@content').get().strip().lower()
+        text = response.xpath('//div[contains(@class,"com-post-content")]/text()').get().strip().lower()
         yield{
             'title':title,
-            'text':text,
-            'comments': list_text_comments,
-            'time':list_date_comments
+            'text':text
             }
+        # store the comments on the first page of the thread
+        for comment in self.parse_comment(response):
+            yield comment
+        # now I follow the other pages on the same thread
 
     def parse(self, response):
-        #filename = 'forum-recenti.html'
-        #with open(filename, 'wb') as f:
-        #    f.write(response.body)
+        # from the main page, follow each comment to its own page
         for topic_ref in response.xpath('//div[@class="discussion-post-item "]//a[@class="discussion-post-item-title"]'):
             yield response.follow(topic_ref, callback=self.parse_topic)
         
